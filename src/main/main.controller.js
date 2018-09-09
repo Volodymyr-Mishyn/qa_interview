247906efbcd6521532784cc598ab429b759c65a2
Volodymyr Mishyn <pti.mishin@gmail.com>
2018-09-09 17:57:32 +0300

    package lock

--- src/main/main.controller.js
+++ src/main/main.controller.js
@@ -1,7 +1,28 @@
 export class MainController {
 
-    constructor() {
+    constructor(MainService) {
         'ngInject';
+        this.MainService = MainService;
         console.log('test-qa');
+        this.list = this.MainService.getList();
+    }
+
+    removeAll(){
+        this.MainService.clearAll();
+    }
+
+    addItem(){
+        let text ='';
+        if (this.text.length>60){
+            text = this.text.substring(0,59);
+        }
+        let item= {title:this.title,text: this.text};
+        this.MainService.addItem(item);
+        this.title='';
+        this.text ='   ';
+    }
+
+    removeItem(item){
+        this.MainService.removeItem(item);
     }
 }
--- src/main/main.service.js
+++ src/main/main.service.js
@@ -1,6 +1,40 @@
 export class MainService {
 
-    constructor() {
+    constructor(localStorageService) {
         'ngInject';
+        this.localStorageService =localStorageService;
+        this.list = [];
+       // this.localStorageService.remove('list');
+        let fromStorage = this.localStorageService.get('list');
+        if (fromStorage){
+            this.list = fromStorage;
+        }
+    }
+
+    getList(){
+        return this.list;
+    }
+
+    addItem(item){
+        item.id = this.list.length+1;
+        this.list.push(item);
+        this.localStorageService.remove('list');
+        this.localStorageService.set('list',this.list);
+    }
+
+    removeItem(item){
+        let index= this.list.indexOf(item);
+        if (index>=0){
+            this.list.splice(index,1);
+            this.localStorageService.remove('list');
+            if (this.list.length>0){
+                 this.localStorageService.set('list',this.list);
+            }
+        }
+    }
+
+    clearAll(){
+        this.list = [];
+        this.localStorageService.remove('list');
     }
 }
